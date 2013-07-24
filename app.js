0fb91632b2b617c95afa3200306ecf62f4076401
Oleg <kalinon7@gmail.com>
2013-07-24 09:55:01 +0400

    user login (doesnt work yet)

--- app.js
+++ app.js
@@ -5,9 +5,8 @@
 
 var express = require('express')
   , mongoose = require('mongoose')
-  , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
+  , bcrypt = require('bcrypt')
   , path = require('path');
 
 var app = express();
@@ -20,6 +19,8 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.cookieSession({secret:';jgfcß∂eiFvb'}));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,6 +28,7 @@ mongoose.connect("mongodb://localhost/todo");
 
 var TodoSchema = new mongoose.Schema({
 	username: String,
+	password: String,
 	ready: [],
 	notReady: []
 }),
@@ -38,10 +40,13 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+
+
+
 app.get('/', function(req, res) {
-	Todo.find({"username": "noname"}, "notReady ready", function(err, docs) {
-		res.render('index', {list:docs});
-	});
+		Todo.find({"username": "noname"}, "notReady ready", function(err, docs) {
+			res.render('index', {list:docs});
+		});
 });
 
 app.get('/init', function(req, res) {
@@ -49,6 +54,8 @@ app.get('/init', function(req, res) {
 		res.send(docs[0]);
 	});
 });
+
+
 
 app.post('/', function(req, res) {
 	Todo.update({ "username": "noname"}, req.body, {upsert: true}, function(err, numberAffected, raw) {
@@ -59,6 +66,68 @@ app.post('/', function(req, res) {
 	res.send(req.body);
 });
 
+app.get('/login', function (req, res) {
+	res.send('Cool hacker, ya?');
+});
+
+
+app.post('/login', function (req, res) {
+  var post = req.body;
+  Todo.count({ "username": post.user}, function(err, count) {
+  	if (count == 0) {
+  		bcrypt.genSalt(10, function(err, salt) {
+    		bcrypt.hash(post.password, salt, function(err, hash) {
+          		Todo.update({ "username": post.user}, {'password': hash}, {upsert: true}, function(err, numberAffected, raw) {
+					if (err) console.log('got an error ' + err);
+					console.log('The number of updated documents was %d', numberAffected);
+					console.log(raw);
+				});
+				req.session.user_id = post.user;
+		    });
+	});
+
+
+
+
+  	} else {
+  		Todo.find({ "username": post.user}, "password", function(err, docs) {
+  			if (err) console.log(err);
+			bcrypt.compare(post.password, docs[0].password, function(err, resp) {
+				if (resp === true) {
+					req.session.user_id = post.user;
+	  				res.redirect("/" + post.user);
+				} else {
+  					setTimeout(function() {res.render('login', {username: post.user, message: "Bad user/pass"})}, 1000);
+  				}
+			});
+  		
+  		});
+
+  	}
+  });
+});
+
+
+
+app.get('/logout', function (req, res) {
+  delete req.session.user_id;
+  res.redirect('/');
+});  
+
+app.get('/:username', function (req, res) {
+	if (!req.session.user_id) {
+		res.render('login', {username: req.params.username});
+	} else {
+		Todo.find({"username": req.session.user_id}, "notReady ready", function(err, docs) {
+			res.render('index', {list:docs, logout: true});
+		});
+	}
+  
+});
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+
